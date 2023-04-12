f00412e5dcb72bb982b39307b88d9e35ab478e3a
NaufalGhifari <85378958+NaufalGhifari@users.noreply.github.com>
2023-04-12 23:23:22 +0800

    Wroking on play-yt-audio interaction
    play-yt-audio is not working. Not sure why, there are no errors nor crash

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ const client = new Client({
         IntentsBitField.Flags.Guilds,
         IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent
+        IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildVoiceStates,
     ]
 });
 
@@ -22,16 +23,64 @@ client.on('ready', (clnt) => {
 /** when a new message is detected */
 client.on('messageCreate', (message) => {
     
-    /** do nothing if the message author is a bot */
-    if (message.author.bot){
-        return
-    }
+    /** Guard clause: do nothing if the message author is a bot */
+    if (message.author.bot) return;
 
     if (message.content === ('hello bot js')){
         message.reply(`Hello ${message.author.username}!`)
     }
+});
 
+client.on('interactionCreate', (interaction) => {
+    /** Guard clause: Only process if the interaction is a chat input command */
+    if (!interaction.isChatInputCommand()) return;
+
+    if(interaction.commandName === 'hello'){
+        interaction.reply(`Hello, ${interaction.member.displayName}!`)
+    }
+
+    if(interaction.commandName === 'sum'){
+        const num_1 = interaction.options.get('first-number')?.value;
+        const num_2 = interaction.options.get('second-number')?.value;
+
+        /** Note to self: the char '?' above is called optional chaining. For optional operations */
+
+        const result = num_1 + num_2;
+        interaction.reply(`The sum of ${num_1} and ${num_2} is: ${result}`);
+    }
+
+    if(interaction.commandName === 'play-yt-audio'){
+        console.log('Processing to play yt audio...')
+
+        /** PICK UP HERE
+         * The interaction does not work, idk why since it doesnt return an error or crash the app
+         */
+
+        const URL = interaction.options.get('yt-link').value;
+        const ytdl = require('ytdl-core');
+        const Discord = require('@discordjs/voice');
+        const stream = ytdl(URL, {filter: 'audioonly'});
+        
+        const player = Discord.createAudioPlayer();
+        const resource = Discord.createAudioResource(stream);
+
+        const connection = Discord.joinVoiceChannel({
+            channelId: interaction.channel.id,
+            guildId: interaction.guild.id,
+            adapterCreator: interaction.guild.voiceAdapterCreator
+
+        });
+
+        player.play(resource);
+        connection.subscribe(player);
+
+        player.on(Discord.AudioPlayerStatus.Idle, () => {
+            connection.destroy();
+        })
+
+    }
 
 });
 
 client.login(process.env.JS_BOT_TOKEN);
+
--- src/register_commands.js
+++ src/register_commands.js
@@ -1,15 +1,64 @@
-require('dotenv').config();
-const {REST, Routes} = require('discord.js');
+/** It is important to precise the path, otherwise dotenv will return 'undefined' */
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
+const {REST, Routes, ApplicationCommandOptionType} = require('discord.js');
 
-console.log(process.env.JS_BOT_TOKEN);
-console.log(process.env.JS_BOT_ID);
-console.log(process.env.FTB_GUILD_ID);
 
 const commands_list = [
     {
-        name: 'hello js bot',
-        description: 'Replies with Hello'
+        name: 'hello',
+        description: 'Let me say hello to you!'
     },
+    {
+        name: 'play-yt-audio',
+        description: 'Takes a youtube link and plays the audio in a VC ;)',
+        options: [
+            {
+                name: 'yt-link',
+                description: 'The URL of a youtube video',
+                type: ApplicationCommandOptionType.String,
+                required: true
+            }
+        ]
+    },
+    {
+        name: 'sum',
+        description: 'Returns the sum of 2 numbers',
+        options: [
+            {
+                name: 'first-number',
+                description: 'The first number to add',
+                type: ApplicationCommandOptionType.Number,
+                choices: [
+                    {
+                        name: 'one',
+                        value: 1
+                    },
+                    {
+                        name: 'two',
+                        value: 2
+                    }
+                ],
+                required: true
+            },
+            {
+                name: 'second-number',
+                description: 'The second number to add',
+                type: ApplicationCommandOptionType.Number,
+                choices: [
+                    {
+                        name: 'one',
+                        value: 1
+                    },
+                    {
+                        name: 'two',
+                        value: 2
+                    }
+                ],
+                required: true
+            }
+        ]
+    }
 ];
 
 const rest = new REST({version:'10'}).setToken(process.env.JS_BOT_TOKEN);
